1dcaa576aaf87afd8be798d0f56ae4586d14ecc5
Umuthan Uyan <umuthan.uyan@ozguryazilim.com.tr>
2019-09-25 13:44:59 +0300

    register reference post type

--- my-references.php
+++ my-references.php
@@ -20,3 +20,22 @@
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
+
+ /* My References Post Type */
+ add_action( 'init', 'my_references_create_reference_post_type' );
+ function my_references_create_reference_post_type() {
+   register_post_type( 'reference',
+     array(
+       'labels' => array(
+         'name' => __( 'References' ),
+         'singular_name' => __( 'Reference' )
+       ),
+       'public' => true,
+       'has_archive' => false,
+       'exclude_from_search' => true,
+       'supports' => array( 'title', 'thumbnail', 'editor', 'custom-fields', 'page-attributes' )
+     )
+   );
+   flush_rewrite_rules();
+ }
+ /* My References Post Type */
