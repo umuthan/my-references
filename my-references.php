91c3cc88e443edbc827da17fa78c6d7aa7a9ba84
Umuthan Uyan <umuthan.uyan@ozguryazilim.com.tr>
2019-09-25 13:44:09 +0300

    plugin's info

---
+++ my-references.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * My References
+ *
+ * @package           My References
+ * @author            Umuthan Uyan
+ * @copyright         2019
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       My References
+ * Plugin URI:        https://wordpress.org/plugins/my-references/
+ * Description:       Reference list for your Wordpress Site
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Umuthan Uyan
+ * Author URI:        http://umuthan.com
+ * Text Domain:       my-references
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
